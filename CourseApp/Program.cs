eb10700139ec08e44064dce99dff968f378e3e1f
User of A309 <a309@it.isuct.ru>
2019-10-14 09:01:00 +0400

    Изменил код

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,24 +4,38 @@ namespace CourseApp
 {
     public class Program
         {
-            public static double Matem(double x, double b = 2.5, double y = 0)
+            public static double Matem(double b, double x)
             {
-                y = (1 + Math.Pow(Math.Sin(Math.Pow(b, 5) + Math.Pow(x, 5)), 2)) / Math.Pow(Math.Pow(b, 3) + Math.Pow(x, 3), 0.33);
+                 var y = (1 + Math.Pow(Math.Sin(Math.Pow(b, 5) + Math.Pow(x, 5)), 2)) / Math.Pow(Math.Pow(b, 3) + Math.Pow(x, 3), 0.33);
                 return y;
             }
 
-        public static void Main(string[] args)
+        public static double[] TaskA(double b, double xn, double xk, double dx)
+        {
+            return new double[0];
+        }
+
+        public static double[] TaskB(double b, double[] x)
+        {
+            var y = new double[x.Length];
+            for (var i = 0; i < x.Length; i++)
             {
-            for (double x = 1.28; x <= 3.28; x += 0.4)
-            {
-                Console.WriteLine(Matem(x));
+                y[i] = Matem(b, x[i]);
             }
 
-            double[] array_x = new double[5] { 1.1, 2.4, 3.6, 1.7, 3.9 };
+            return y;
+        }
 
-            for (int i = 0; i < 5; i++)
+        public static void Main(string[] args)
+        {
+        const double b = 2.5;
+        var resSingle = Matem(b, 4);
+        Console.WriteLine(resSingle);
+        var x = new double[] { 1.1, 2.4, 3.6, 1.7, 3.9 };
+        var taskBRes = TaskB(b, x);
+            foreach (var item in taskBRes)
             {
-                Console.WriteLine(Matem(array_x[i]));
+                Console.WriteLine($"y = {item}");
             }
         }
     }
